e3789594a808ef93fecc8ecb9ae5a5fc1e8700f0
Fuad Hasan <fuadgit@gmail.com>
2024-08-15 20:55:38 +0600

    Update code
    Computer play this game automatically

--- Python/guess_number.py
+++ Python/guess_number.py
@@ -2,9 +2,13 @@ import random
 
 number = random.randint(1, 1000)
 attempts = 0
+low = 1
+higth = 1000
 
 while True:
-       input_number = int(input("Guess the number(between 1 and 1000)\n~ "))
+       ("Guess the number(between 1 and 1000)\n~ ")
+       input_number = (low+higth) // 2
+       print("My guess number is", input_number)
        attempts += 1
        
        if input_number == number :
@@ -12,7 +16,9 @@ while True:
               break
        if input_number > number:
               print("Incoorect! Please try to guess a smaller number")
+              higth = input_number - 1
        else:
               print("Incoorect! Please try to guess a larger number")   
+              low = input_number + 1
               
-print("You tried", attempts, "times to find the correct number.")                         
+print("You tried", attempts, "times to find the correct number.")     
